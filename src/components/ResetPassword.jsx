f3ac5b17c4fedd7d53bfde7d7873dd2934697f62
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-12-22 00:47:44 +0530

    pushed

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -7,7 +6,7 @@ function ForgetPasword() {
   const [email, setEmail] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const res = axios.post('https://localhost:4000/sendpasswordlink',{Email:email})
+    const res = axios.post('https://bus-server-chi.vercel.app/sendpasswordlink', { Email: email })
     if (res) {
       setEmail('')
       toast.success("Password reset link send succesfully to your Email")
@@ -16,45 +15,45 @@ function ForgetPasword() {
     }
   }
   return (
-   
-      <main>
-        <section className="login ptb-50">
-          <div className="container">
-            <div className="row">
-              <div className="col-12 col-md-10 col-lg-6 col-xxl-4 mx-auto">
-                <div className="title-section text-center mb-4">
-                  <h1 className="heading heading-decor mb-0">
-                    Reset your password
-                  </h1>
-                </div>
-                <form action="/Login" className="row form">
-                  <div className="col-12 mb-4">
-                    <label className="mb-0">EMAIL</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                  <div className="col-12 text-center pt-1">
-                    <button
-                      type="button"
-                      className="btn btn-hover btn-custom btn-primary px-4"
-                    >
-                      SUBMIT
-                    </button>
-                    <div className="mt-3">
-                      <Link to="/Login" className="text-muted btn p-0 fs-6">
-                        Cancel
-                      </Link>
-                    </div>
-                  </div>
-                </form>
+
+    <main className="border-top">
+      <section className="login ptb-50">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-10 col-lg-6 col-xxl-4 mx-auto">
+              <div className="title-section text-center mb-4">
+                <h1 className="heading heading-decor mb-0">
+                  Reset your password
+                </h1>
               </div>
+              <form onSubmit={handleSubmit} className="row form">
+                <div className="col-12 mb-4">
+                  <label className="mb-0">EMAIL</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="col-12 text-center pt-1">
+                  <button
+                    type="submit"
+                    className="btn btn-hover btn-custom btn-primary px-4"
+                  >
+                    SUBMIT
+                  </button>
+                  <div className="mt-3">
+                    <Link to="/Login" className="text-muted btn p-0 fs-6">
+                      Cancel
+                    </Link>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-        </section>
-      </main>
+        </div>
+      </section>
+    </main>
   );
 }
 
